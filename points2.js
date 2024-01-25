f068fe04eeedac54c772319538fefc9e3bf4466e
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-01-26 02:48:01 +0900

    Add files via upload

---
+++ points2.js
@@ -0,0 +1,10 @@
+var eq_points= {
+"type": "FeatureCollection",
+"name": "eqarthquakes_ger",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": 1, "EV_DATE": 8130000, "EV_TIME": 0.0, "LAT": 50.8, "LON": 6.1, "DEPTH": null, "DEP_COMM": null, "MAG": null, "MAG_TYPE": null, "INTENSITY": 4.0, "INT_SCALE": "MSK", "ISO_DATE": null, "ISO_TIME": "00:00:00.000", "PT_SIZE": 2.888, "EV_TYPE": "ke", "EXPL_DE": "tektonisch", "EXPL_EN": "known earthquake", "SOURCE": "LEY11", "EV_ID": 1 }, "geometry": { "type": "Point", "coordinates": [ 6.1, 50.8 ] } },
+{ "type": "Feature", "properties": { "fid": 2, "EV_DATE": 8230000, "EV_TIME": 0.0, "LAT": 50.8, "LON": 6.1, "DEPTH": null, "DEP_COMM": null, "MAG": null, "MAG_TYPE": null, "INTENSITY": 7.0, "INT_SCALE": "MSK", "ISO_DATE": null, "ISO_TIME": "00:00:00.000", "PT_SIZE": 4.934, "EV_TYPE": "ke", "EXPL_DE": "tektonisch", "EXPL_EN": "known earthquake", "SOURCE": "LEY11", "EV_ID": 2 }, "geometry": 
+{ "type": "Feature", "properties": { "fid": 22693, "EV_DATE": 20190919, "EV_TIME": 31919.46, "LAT": 48.289, "LON": 9.029, "DEPTH": 12.0, "DEP_COMM": null, "MAG": 2.0, "MAG_TYPE": "ML", "INTENSITY": null, "INT_SCALE": null, "ISO_DATE": null, "ISO_TIME": "03:19:19.460", "PT_SIZE": 2.0, "EV_TYPE": "ke", "EXPL_DE": "tektonisch", "EXPL_EN": "known earthquake", "SOURCE": "LED", "EV_ID": 1190919001 }, "geometry": { "type": "Point", "coordinates": [ 9.029, 48.289 ] } }
+]
+}
