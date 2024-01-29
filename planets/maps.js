4983e2a82dc8b31b8b0edf10cc5116af2b7416da
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-01-29 17:42:00 +0900

    Update maps.js

--- planets/maps.js
+++ planets/maps.js
@@ -2,7 +2,7 @@
 
 // Same but for moon map
 let moonMap = L.map('moon-map')
-moonMap.setView([13.3160, 25.2510], 8)
+moonMap.setView([13.3160, 25.2510], 6)
 
 var baselayer = new L.tileLayer('https://cartocdn-gusc.global.ssl.fastly.net/opmbuilder/api/v1/map/named/opm-moon-basemap-v0-1/all/{z}/{x}/{y}.png')
     .addTo(moonMap)
