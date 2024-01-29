8c16c37919ea4eb3fbee71376c15a3f8ba5d99c4
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-01-29 20:03:56 +0900

    Update maps.js

--- planets/maps.js
+++ planets/maps.js
@@ -1,9 +1,12 @@
 // https://www.openplanetary.org/opm/basemaps
-
 // Same but for moon map
+
 let moonMap = L.map('moon-map')
 moonMap.setView([13.3160, 25.2510], 6)
 
 var baselayer = new L.tileLayer('https://cartocdn-gusc.global.ssl.fastly.net/opmbuilder/api/v1/map/named/opm-moon-basemap-v0-1/all/{z}/{x}/{y}.png')
     .addTo(moonMap)
     .setZIndex(0);
+
+var marker = L.marker([13.3160, 25.2510]).addTo(moonMap); // マーカーの座標を設定
+marker.bindPopup("SLIM JAPAN").openPopup(); // マーカーにポップアップを追加
